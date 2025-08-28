b9356c2fe288ce6e10739b6a83690b1aa7733a00
LuckyQu <super.lucky.qu@gmail.com>
2025-08-28 10:25:46 +0800

    新增一道题的JS解法

---
+++ 374_猜数字大小/main.js
@@ -0,0 +1,37 @@
+//我们正在玩猜数字游戏。猜数字游戏的规则如下：
+//
+// 我会从 1 到 n 随机选择一个数字。 请你猜选出的是哪个数字。（我选的数字在整个游戏中保持不变）。
+//
+// 如果你猜错了，我会告诉你，我选出的数字比你猜测的数字大了还是小了。
+//
+// 你可以通过调用一个预先定义好的接口 int guess(int num) 来获取猜测结果，返回值一共有三种可能的情况：
+//
+// -1：你猜的数字比我选出的数字大 （即 num > pick）。
+// 1：你猜的数字比我选出的数字小 （即 num < pick）。
+// 0：你猜的数字与我选出的数字相等。（即 num == pick）。
+// 返回我选出的数字。
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let a = n/2
+    let b = guess(a)
+    for (; b !== 0; b = guess(a)){
+        if (b === 1){
+            a = 1.5 * a
+        }else {
+            a /= 2
+        }
+    }
+    return a
+};
