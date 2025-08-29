0a152594af1b57aab57d1e50f91d38d752992e8c
LuckyQu <super.lucky.qu@gmail.com>
2025-08-29 10:11:16 +0800

    新增一道题的JS解法

---
+++ 136_只出现一次的数字/main.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+var singleNumber = function(nums) {
+    let sum = 0
+    for (let i = 0; i < nums.length; i++){
+        sum ^= nums[i]
+    }
+    return sum
+};
